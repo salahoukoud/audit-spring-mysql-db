458a1da4d9c0cacbf863baf2689c450259d82fa2
salah oukoud <salahoukoud@gmail.com>
2018-07-02 12:16:17 +0100

    milestone change #rename artifact #remove unused dependencies #remove unused class #add new spring application main class's

---
+++ src/main/java/com/saoah/envers/SampleApplication.java
@@ -0,0 +1,11 @@
+package com.saoah.envers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication {
+    public static void main(String[] args) {
+        new SpringApplication().run(SampleApplication.class, args);
+    }
+}
