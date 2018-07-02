f003cf9832caca92cd325456cf49143960737e7c
salah oukoud <salahoukoud@gmail.com>
2018-07-02 17:05:39 +0100

    Comment CustomerHistory.java

--- src/main/java/com/saoah/envers/model/CustomerHistory.java
+++ src/main/java/com/saoah/envers/model/CustomerHistory.java
@@ -5,6 +5,10 @@ import org.hibernate.envers.RevisionType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * The Auditing Table <b>customer_aud</b> contains the <b>customer entity data</b> of the revision,
+ * the <b>revision entity</b> and the <b>revision type</b>
+ */
 @AllArgsConstructor
 @Getter
 public class CustomerHistory {
