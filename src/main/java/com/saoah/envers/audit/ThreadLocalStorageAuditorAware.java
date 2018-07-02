0dfbf4dc37510e7852bca109110b764a075f0309
salah oukoud <salahoukoud@gmail.com>
2018-07-02 16:32:49 +0100

    Determining the Current Auditor (username as a String)

---
+++ src/main/java/com/saoah/envers/audit/ThreadLocalStorageAuditorAware.java
@@ -0,0 +1,36 @@
+package com.saoah.envers.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+/**
+ * class implement {@link AuditorAware} interface for providing the username at runtime
+ */
+@Component
+public class ThreadLocalStorageAuditorAware implements AuditorAware<String> {
+
+    private String systemUsername = "system", username;
+
+    @Override
+    public String getCurrentAuditor() {
+        // for web app usage
+//        username = (String) RequestContextHolder.currentRequestAttributes()
+//                .getAttribute("username", RequestAttributes.SCOPE_REQUEST);
+
+        // Spring Security
+//        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        if (authentication != null && authentication.isAuthenticated())
+//            username = authentication.getName();
+
+        if (username.equals(null) || username.isEmpty()) {
+            username = systemUsername;
+        }
+
+//        LOGGER.info(String.format("Current auditor is >>> %s", username));
+        return username;
+    }
+}
