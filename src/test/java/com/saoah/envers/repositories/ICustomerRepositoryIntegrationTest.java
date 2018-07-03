06e76cbd444cffdfdfb90fb4e0dd4da526ea7895
salah oukoud <salahoukoud@gmail.com>
2018-07-03 15:10:03 +0100

    Test #Add simple UT class #Add simple IT class #Add application.yml resource file

---
+++ src/test/java/com/saoah/envers/repositories/ICustomerRepositoryIntegrationTest.java
@@ -0,0 +1,47 @@
+package com.saoah.envers.repositories;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.saoah.envers.SampleApplication;
+import com.saoah.envers.model.Customer;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SampleApplication.class)
+public class ICustomerRepositoryIntegrationTest {
+
+    private static Long idCustomer;
+
+    @Autowired
+    private ICustomerRepository customerRepository;
+
+    @Test
+    public void test_save() {
+        Customer customer = new Customer("Save", "Test");
+        this.customerRepository.save(customer);
+        assertNotNull(customer.getId());
+        idCustomer = customer.getId();
+        System.out.println(" CUSTOMER : " + customer);
+    }
+
+    @Test
+//    @Transactional
+    public void test_update() throws InterruptedException {
+        Customer customer = this.customerRepository.findOne(idCustomer);
+        assertNotNull(customer);
+        System.out.println(customer);
+        customer.setFirstName("Edited");
+        Thread.sleep(5000);
+        this.customerRepository.save(customer);
+        System.out.println(customer);
+//        Thread.sleep(5000);
+        customer= this.customerRepository.findOne(idCustomer);
+        System.out.println(customer);
+
+    }
+}
---
+++ src/test/java/com/saoah/envers/repositories/ICustomerRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.saoah.envers.repositories;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.saoah.envers.model.Customer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest(showSql = true)
+//@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Commit // because @datajpatest Rolled back transaction
+public class ICustomerRepositoryTest {
+
+    private static Long idCustomer;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private ICustomerRepository customerRepository;
+
+    @Test
+    public void test_findAll() {
+        List<Customer> customers = customerRepository.findAll();
+
+        assertNotNull("TOTO", customers);
+    }
+
+    @Test
+    public void test_save() {
+        Customer customer = new Customer("Save", "Test");
+        this.customerRepository.save(customer);
+        assertNotNull(customer.getId());
+        idCustomer = customer.getId();
+        System.out.println("ID CUSTOMER : " + customer.getId());
+    }
+
+    @Test
+    public void test_findById() {
+        Customer customer = new Customer("TOTO", "TITI");
+        this.entityManager.persist(customer);
+        customer = customerRepository.findOne(idCustomer);
+        assertNotNull(customer);
+        assertEquals(customer.getFirstName(), "TOTO");
+        assertEquals(customer.getLastName(), "TITI");
+    }
+}
